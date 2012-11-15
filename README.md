ecfb3f9e5cc0624a5d0c479e7692e4a3e0b884d0
Anthony Eden <anthonyeden@gmail.com>
2012-11-15 19:54:58 +0100

    FizzBuzz in Erlang.

---
+++ README.md
@@ -0,0 +1,11 @@
+FizzBuzz implementation in Erlang.
+
+To compile and run:
+
+    $ erl
+    1> compile("fizzbuzz").
+    {ok,fizzbuzz}
+    2> fizzbuzz:run().
+    ...
+   
+<ctrl-C> twice to exit.
---
+++ fizzbuzz.erl
@@ -0,0 +1,21 @@
+% Write a program that prints out the numbers from 1 through 100, but…
+%  For numbers that are multiples of 3, print “Fizz” instead of the number.
+%  For numbers that are multiples of 5, print “Buzz” instead of the number
+%  For numbers that are multiples of both 3 and 5, print “FizzBuzz” instead of the number.
+% That’s it!
+
+-module(fizzbuzz).
+
+-export([run/0]).
+
+-include_lib("eunit/include/eunit.hrl").
+
+run() -> run(1).
+run(N) -> lists:foreach(fun(S) -> io:format("~s~n", [S]) end, run(N, [])).
+run(N, Res) when N > 100 -> Res;
+run(N, Res) when (N rem 3 =:= 0) and (N rem 5 =:= 0) -> run(N + 1, Res ++ ["FizzBuzz"]);
+run(N, Res) when N rem 3 =:= 0 -> run(N + 1, Res ++ ["Fizz"]);
+run(N, Res) when N rem 5 =:= 0 -> run(N + 1, Res ++ ["Buzz"]);
+run(N, Res) -> run(N + 1, Res ++ [integer_to_list(N)]).
+
+fizz_test() -> [1, 2, "Fizz"] = run(3, []).
